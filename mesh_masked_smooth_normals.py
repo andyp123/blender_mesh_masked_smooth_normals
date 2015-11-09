40818f4efeebaf22ce1bad418014da311e5bddaf
Andrew Palmer <andyp.123@gmail.com>
2015-11-10 00:53:52 +0900

    Add flip and harden selection
    Flip and harden face selections. Would like to make harden work on
    edge/vertex selections too...

--- mesh_masked_smooth_normals.py
+++ mesh_masked_smooth_normals.py
@@ -19,16 +19,18 @@ import bpy
 import bmesh
 import mathutils
 
+
 # addon information
 bl_info = {
     "name": "Masked Soften/Harden Normals",
     "author": "Andrew Palmer",
-    "version": (0, 0, 2),
+    "version": (0, 0, 3),
     "blender": (2, 75, 0),
     "location": "",
     "description": "Soften/Harden the vertex normals of a mesh using the current selection as a mask that defines which normals are affected.",
     "category": "Mesh"
 }
+
 
 # OPERATES IN OBJECT MODE ONLY
 def set_smooth_normals(mesh_data, vertex_indices, vertex_normals):
@@ -38,7 +40,7 @@ def set_smooth_normals(mesh_data, vertex_indices, vertex_normals):
     me.calc_normals_split()
     
     # create a structure that matches the required input of the normals_split_custom_set function
-    normals = [mathutils.Vector()] * len(me.loops)
+    clnors = [mathutils.Vector()] * len(me.loops)
 
     for loop in me.loops:
         vertex_normal = loop.normal
@@ -47,14 +49,77 @@ def set_smooth_normals(mesh_data, vertex_indices, vertex_normals):
         if vertex_index in vertex_indices:
             idx = vertex_indices.index(vertex_index)
             vertex_normal = vertex_normals[idx]
-        normals[loop.index] = vertex_normal
+        clnors[loop.index] = vertex_normal
 
-    me.normals_split_custom_set(normals)
+    me.normals_split_custom_set(clnors)
+
+
+# OPERATES IN OBJECT MODE ONLY
+def flip_normals(mesh_data):
+    me = mesh_data
+
+    me.calc_normals_split()
+    clnors = [mathutils.Vector(loop.normal) for loop in me.loops]
+
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.flip_normals()
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    for p in me.polygons:
+        if p.select:
+            # flip the normals of the selected polygon
+            ls = p.loop_start
+            le = ls + p.loop_total
+            clnors[ls:le] = [-n for n in clnors[ls:le]]
+            # revert winding order :(
+            ls = p.loop_start + 1
+            le = ls + p.loop_total - 1
+            clnors[ls:le] = reversed(clnors[ls:le])
+
+    me.normals_split_custom_set(clnors)
+
+
+# OPERATES IN EDIT MODE ONLY
+def harden_normals(mesh_data):
+    """Harden normals based on selected faces"""
+
+    me = mesh_data
+
+    me.calc_normals_split()
+    clnors = [loop.normal for loop in me.loops]
+
+    for p in me.polygons:
+        if p.select:
+            ls = p.loop_start
+            le = ls + p.loop_total
+            clnors[ls:le] = list([p.normal] * p.loop_total)
+
+    me.normals_split_custom_set(clnors)
+
+
+# OPERATES IN EDIT MODE ONLY
+def set_specific_normal_vector(mesh_data, normal):
+    """Set the selected normals to a specific direction vector"""
+
+    me = mesh_data
+
+    me.calc_normals_split()
+    clnors = [loop.normal for loop in me.loops]
+
+    normal.normalize()
+
+    for p in me.polygons:
+        if p.select:
+            ls = p.loop_start
+            le = ls + p.loop_total
+            clnors[ls:le] = list([normal] * p.loop_total)
+
+    me.normals_split_custom_set(clnors)
 
 
 # OPERATES IN EDIT MODE ONLY
 def get_smoothed_vertex_normals(mesh_data, smooth_mode="face"):
-    """Smooth normals based on selected vertices, with option to calculate normals based only on selected faces"""
+    """Smooth normals based on selection"""
 
     bm = bmesh.from_edit_mesh(mesh_data)
 
@@ -89,7 +154,9 @@ def get_smoothed_vertex_normals(mesh_data, smooth_mode="face"):
 
     return {'vertex_indices': vertex_indices, 'vertex_normals': vertex_normals}
 
+
 class MaskedSoftenNormals(bpy.types.Operator):
+    """Smooth custom normals based on selection"""
     bl_idname = "mesh.masked_soften_normals"
     bl_label = "Masked Soften Normals"
     bl_options = {'REGISTER', 'UNDO'}
@@ -125,14 +192,22 @@ class MaskedSoftenNormals(bpy.types.Operator):
         obj = context.object  
         return obj is not None and obj.mode == 'EDIT' 
 
-# TODO: Make a harden normals tool that works in the same way as smooth normals
 
+# TODO: Make a harden normals tool that works in the same way as smooth normals
 class MaskedHardenNormals(bpy.types.Operator):
+    """Harden custom normals based on selection"""
     bl_idname = "mesh.masked_harden_normals"
     bl_label = "Masked Harden Normals"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
+        mesh_data = context.object.data
+        mesh_data.use_auto_smooth = True
+
+        bpy.ops.object.mode_set(mode="OBJECT")
+        harden_normals(mesh_data)
+        bpy.ops.object.mode_set(mode="EDIT")
+
         return {'FINISHED'}
 
     @classmethod  
@@ -140,13 +215,36 @@ class MaskedHardenNormals(bpy.types.Operator):
         obj = context.object  
         return obj is not None and obj.mode == 'EDIT' 
 
+
+class FlipCustomNormals(bpy.types.Operator):
+    """Flip active mesh's normals, including custom ones"""
+    bl_idname = "mesh.masked_flip_custom_normals"
+    bl_label = "Flip Custom Normals"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        obj = context.object  
+        return obj is not None and obj.mode == 'EDIT' 
+
+    def execute(self, context):
+        mesh_data = context.object.data
+        mesh_data.use_auto_smooth = True
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+        flip_normals(mesh_data)
+        bpy.ops.object.mode_set(mode='EDIT')
+
+        return {'FINISHED'}
+
+
 class SoftenHardenNormalsPanel(bpy.types.Panel):
     """COMMENT"""
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     bl_context = "mesh_edit"
     bl_category = "Shading / UVs"
-    bl_label = "Soften / Harden"
+    bl_label = "Custom Normals"
 
     def draw(self, context):
         layout = self.layout
@@ -155,17 +253,21 @@ class SoftenHardenNormalsPanel(bpy.types.Panel):
         row = layout.row(align=True)
         row.operator("mesh.masked_soften_normals", text="Soften")
         row.operator("mesh.masked_harden_normals", text="Harden")
+        row = layout.row(align=True)
+        row.operator("mesh.masked_flip_custom_normals", text="Flip Direction")
 
 
 # operator registration
 def register():  
     bpy.utils.register_class(MaskedSoftenNormals)
-    # bpy.utils.register_class(MaskedHardenNormals)
+    bpy.utils.register_class(MaskedHardenNormals)
+    bpy.utils.register_class(FlipCustomNormals)
     bpy.utils.register_class(SoftenHardenNormalsPanel)
 
 def unregister():
     bpy.utils.unregister_class(MaskedSoftenNormals)
-    # bpy.utils.unregister_class(MaskedHardenNormals)
+    bpy.utils.unregister_class(MaskedHardenNormals)
+    bpy.utils.unregister_class(FlipCustomNormals)
     bpy.utils.unregister_class(SoftenHardenNormalsPanel)
   
 if __name__ == "__main__":  
